5753f3d6e3c32fd83a39bab7aac81c1cf726abb1
Entlino <111046353+Entlino@users.noreply.github.com>
2022-09-08 11:28:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Security.AccessControl;
+byte guesszahl;
+byte versuche = 0;
+string runde;
+start:
+byte randomzahlen = Convert.ToByte(new Random().Next(0, 100));
+do
+{
+    Console.Write("Sie können nun eine Zahl zwischen 0 und 100 eingeben:");
+     guesszahl = Convert.ToByte(Console.ReadLine());
+    Console.WriteLine("--------------------------------------------------------");
+    versuche++;
+    if (guesszahl < randomzahlen)
+    {
+        Console.WriteLine("Die gesuchte Zahl ist grösser als die geratene Zahl.");
+        Console.WriteLine("");
+    }
+    else if (guesszahl > randomzahlen)
+    {
+        Console.WriteLine("Die gesuchte Zahl ist kleiner als die geratete Zahl.");
+        Console.WriteLine("");
+    }
+            
+    else if (guesszahl == randomzahlen)       
+    {
+    Console.WriteLine("Sehr gut!, Sie haben die Gewünschste Zahl erraten!");
+    Console.WriteLine("Sie haben " + versuche + " Versuche gebraucht");
+    Console.WriteLine("--------------------------------------------------------");
+    Console.Write("Möchten Sie nochmal eine runde Spielen? [ja/nein]");
+    runde = Convert.ToString(Console.ReadLine());
+    }
+if (runde.ToLower == "ja")
+{
+    goto start;
+}
+else if (runde.ToLower == "nein")
+{
+    Environment.Exit(0);
+}
+
+} while (guesszahl != randomzahlen);
